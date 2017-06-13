a39d3e9664006ef42f529bd06cde1ca95b69869b
ganzw <ganzw@guahao.com>
2017-06-13 17:05:24 +0800

    feat first commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # atpl-cli
-node.js template init
+init atpl project   
+
+   
+# install  
+   
+```
+npm install -g atpl-cli   
+```
+    
+# start
+  
+```
+mkdir project   
+cd project  
+atpl init   
+```   
+   
+# options    
+  
+-h, --help     output usage information    
+-V, --version  output the version number   
+
+# commands   
+   
+init|i   Generate a new project    
---
+++ commands/init.js
@@ -0,0 +1,34 @@
+const { prompt } = require('inquirer')
+const { writeFile } = require('fs')
+const chalk = require('chalk')
+const download = require('download-git-repo')
+const ora = require('ora')
+
+const tips = [
+  {
+    type: 'input',
+    name: 'confirm',
+    message: 'Are you sure download project on current directory (Y/n):'
+  }
+]
+
+module.exports = prompt(tips).then(({confirm}) => {
+  if(confirm === 'n' || confirm === 'N') {
+    return true;
+  }
+  const gitPlace = "baixuexiyang/node-atpl-express"
+  const gitBranch = "master"
+  const spinner = ora('init project...')
+  const place = "node-atpl-express"
+  spinner.start()  
+  download(`${gitPlace}#${gitBranch}`, `${__dirname}/../${place}`, (err) => {
+    if (err) {
+      console.log(chalk.red(err))
+      process.exit()
+    }
+    spinner.stop()
+    console.log(chalk.green('New project has been initialized successfully!'))
+  });
+});
+
+
